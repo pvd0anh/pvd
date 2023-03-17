e3a2a1e96fe038517f453c75a19a619f72f89903
Pham Van Doanh <pvdoanh.ce@gmail.com>
2023-03-17 14:22:56 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pvd',
+      version='0.0.1',
+      description='Library for save the time',
+      url='https://github.com/pvd0anh/pvd',
+      author='PV Doanh',
+      author_email='pvdoanh.ce@gmail.com',
+      license='MIT',
+      packages=['pvd'],
+      zip_safe=False)
